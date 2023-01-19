1d20ae15aa39c5f317441e3d8df8d9533e83756d
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-01-19 16:57:01 +0000

    Create models for dynamic property

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,34 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+export class FaceSnapComponent implements OnInit{
+
+  @Input() faceSnap!: FaceSnap;
+
+
   ohsnap!: string;
 
   ngOnInit() {
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis tout petit !';
-    this.createdDate = new Date();
-    this.snaps = 6;
+   
     this.ohsnap = 'Oh snap'
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
 
   onAddSnap(){
     if (this.ohsnap==='Oh snap'){
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.ohsnap='Unsnap';
     }
     else{
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.ohsnap='Oh snap';
     };
     

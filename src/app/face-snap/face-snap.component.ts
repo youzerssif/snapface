9ee1445e73fc0c84b6af7c26f60f0bba64bce183
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-01-19 09:24:04 +0000

    Display data with interpolation and attribut

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }

41ea398c646d726d3d7c1423a32fde5ea7c519fe
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-01-23 23:32:16 +0000

    Create service for data manipulation

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from '../models/face-snap.model'
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
 
 
 @Component({
@@ -10,31 +12,10 @@ import { FaceSnap } from '../models/face-snap.model'
 export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Paris'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'la montagne'
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 0
-      }
-    ];
+  constructor(private faceSnapsService: FaceSnapsService) { }
+
+  ngOnInit(): void {
+    this.faceSnaps = this.faceSnapsService.faceSnaps;
+    // console.log(this.faceSnapsService.getAllSnaps())
   }
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaceSnapsService {
+  faceSnaps: FaceSnap[] = [
+    {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Paris'
+    },
+    {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'la montagne'
+    },
+    {
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    }
+  ];
+//   getAllSnaps(){
+//     return this.faceSnaps
+//   }
+}

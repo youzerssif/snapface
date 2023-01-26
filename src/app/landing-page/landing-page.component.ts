67e28576d362f4911595add4b15eab7b567ce77f
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-01-26 16:15:33 +0000

    Add router by button

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+  
+  constructor(private router: Router) { }
 
+
+  onContinue() {
+    this.router.navigateByUrl('facesnaps');
+}
 }
